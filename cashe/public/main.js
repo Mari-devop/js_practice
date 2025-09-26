3b60281fcf45fdc326d1b03bfb4053405725dfab
Mari Filon <mari.philon@gmail.com>
2025-09-26 21:27:04 +0100

    add new task

---
+++ cashe/public/main.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js", { scope: "/" })
+      .then((reg) => console.log("Service worker registered", reg.scope))
+      .catch((err) => console.error("Service worker registration failed", err));
+  });
+}
